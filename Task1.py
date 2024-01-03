1a1b61efbc22440268354093d7a703d14a1398bb
Imperator5544 <136652333+Imperator5544@users.noreply.github.com>
2024-01-03 16:27:54 +0200

    Commit task 1(2)

--- Task1.py
+++ Task1.py
@@ -4,20 +4,20 @@ print("Enter any three numbers")
 Залежно від вибору користувача програма виводить на екран максимум із трьох, 
 мінімум із трьох або середньоарифметичне трьох чисел.
 """
-first_number = float(input("Enter first number: "))
-second_number = float(input("Enter second number"))
-third_number = float(input("Enter third number"))
+number_first = float(input("Enter first number: "))
+number_second = float(input("Enter second number"))
+number_third = float(input("Enter third number"))
 
 operation = input("Select an operation")
 
 if operation == "min":
-    result = min(first_number, second_number, third_number)
+    result = min(number_first, number_second, number_third)
     print(f"Minimum {result}")
 elif operation == "max":
-    result = max(first_number, second_number, third_number)
+    result = max(number_first, number_second, number_third)
     print(f"Maximum {result}")
 elif operation == "avg":
-    result = (first_number + second_number + third_number) / 3
+    result = (number_first + number_second + number_third) / 3
     print(f"Average {result}")
 
 
